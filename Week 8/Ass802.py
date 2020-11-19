d9b5f07d46fbd90872e384d01e62945a926d6d3b
mthakk3793 <74619685+mthakk3793@users.noreply.github.com>
2020-11-19 13:10:39 -0600

    Update and rename plotViewer.py to Week 8/Ass802.py

--- Week 8/Ass802.py
+++ Week 8/Ass802.py
@@ -1,7 +1,4 @@
-# Maharshi Thakkar
-# 08/13/2020
-# "I have not given or received any unauthorized assistance on this assignment."
-# https://youtu.be/fa4lvf0Jq74
+
 
 from plotGenerator import InteractivePlotGenerator, SimplePlotGenerator, RandomPlotGenerator
 
